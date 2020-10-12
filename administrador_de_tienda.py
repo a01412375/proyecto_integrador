a54376146cb9332f019fab26e3e7cb99a69f7b91
a01412375 <a01412375@itesm.mx>
2020-10-11 23:55:23 -0500

    Menu "1) Registrar ventas" casi completo. Archivos de texto agregados.

--- administrador_de_tienda.py
+++ administrador_de_tienda.py
@@ -4,7 +4,7 @@
 # Juan Daniel
 # 10/2020
 
-# Definición de funciones
+# Definición de funciones generales
 
 
 def pedir_entero(num_min, num_max, peticion=''):
@@ -13,7 +13,7 @@ def pedir_entero(num_min, num_max, peticion=''):
     while True:
         # Solo acepta números enteros
         try:
-            numero = int(input(f"{peticion} >> "))
+            numero = int(input(f"\n{peticion} >> "))
         except:
             print("Ingrese un número válido.")
             continue
@@ -25,10 +25,24 @@ def pedir_entero(num_min, num_max, peticion=''):
             print("Ingrese un número valido.")
 
 
-def pasar_texto_a_matriz(texto):
+def pedir_eleccion(matriz, tipo_de_columna, peticion):
+    ''' Muestra al usuario una lista de opciones y regresa su eleccion en un entero. '''
+
+    # Obtiene la lista de opciones a desplegar
+    lista = obtener_columna(matriz, tipo_de_columna)
+    cantidad_lista = len(lista)
+    mostrar_lista_en_menu(lista)
+
+    eleccion = pedir_entero(1, cantidad_lista, f"{peticion} (1-{cantidad_lista})")
+
+    return eleccion
+
+
+def pasar_texto_a_matriz(nombre_texto):
     ''' Regresa una matriz apartir de un archivo de texto. '''
 
     matriz = []
+    texto = open(nombre_texto, "r")
 
     for linea in texto:
         # Ignora los comentarios
@@ -38,17 +52,59 @@ def pasar_texto_a_matriz(texto):
         linea = linea.replace("\n", "")
         matriz.append(linea.split(","))
 
+    texto.close()
+
     return matriz
 
 
-def elegir_columna(matriz, columna):
-    pass
+def elegir_fila(matriz, num_fila):
+    ''' Regresa una lista con la columna seleccionada de una matriz. '''
+
+    return matriz[num_fila - 1]
+
+
+def elegir_columna(matriz, num_columna):
+    ''' Regresa una lista con la fila seleccionada de una matriz. '''
+
+    lista = []
+
+    for fila in matriz:
+        lista.append(fila[num_columna - 1])
+
+    return lista
+
+
+def mostrar_lista_en_menu(lista):
+    ''' Muestra al usuario una lista de todos los vendedores. '''
+
+    print('')
+    i = 1
+    for nombre in lista:
+        print(f"{i}) {nombre}")
+        i += 1
+
+
+def obtener_columna(matriz, tipo_de_columna):
+    ''' Regresa una lista de la columna de un archivo de texto. '''
+
+    matriz = elegir_columna(matriz, tipo_de_columna)
+
+    return matriz
+
+
+def editar_matriz_suma(matriz, fila, columna, valor_a_sumar):
+    ''' Regresa una matriz con un valor alterado por una suma. '''
+
+    matriz[fila][columna] = str(int(matriz[fila][columna]) + valor_a_sumar)
+
+
+# Definición de funciones específicas
 
 
 def mostrar_menu():
     ''' Muestra al usuario el menú principal del programa. '''
 
-    print(f"1) Registrar ventas\n"
+    print(f"\n1) Registrar ventas\n"
           f"2) Registrar llegada de artículos al almacén\n"
           f"3) Consultar datos del inventario\n"
           f"4) Consultar datos de las ventas\n"
@@ -56,35 +112,74 @@ def mostrar_menu():
           f"6) Mostrar reportes de ventas por artículo")
 
 
-def menu_registrar_ventas():
-    ''' Despliega el menú 1. '''
-    pass    # Muestra la lista de vendedores
-    pass    # pedir_entero(1, numero de vendedores)
+def mostrar_menu_registrar_ventas():
+    '''
+    Despliega el menú 1.
+    Permite a un vendedor registar la venta de un producto por su parte.
+    '''
+
+    eleccion_vendedor = pedir_eleccion(matriz_vendedores, columnas_vendedores.get("nombre"), "Seleccione su nombre")
+    id_vendedor = eleccion_vendedor - 1
+
+    eleccion_producto = pedir_eleccion(matriz_productos, columnas_productos.get("nombre"),
+                                       "Seleccione el producto a vender")
+    id_producto = eleccion_producto - 1
+
+    # Obtiene la cantidad disponible del producto
+    cantidad = obtener_columna(matriz_productos, columnas_productos.get("cantidad"))
+    cantidad_producto = int(cantidad[id_producto])
+
+    # Avisa si el producto se encuentra agotado
+    if cantidad_producto < 1:
+        print("\nEl producto se encuentra agotado.")
+        input("Da click para continuar.")
+        return
+
+    eleccion_cantidad = pedir_entero(1, cantidad_producto, f"Cantidad del producto a vender (1-{cantidad_producto})")
+
+    # Actualiza los cambios realizados a las matrices
+    editar_matriz_suma(matriz_ventas, id_vendedor, id_producto + 1, eleccion_cantidad)
+    editar_matriz_suma(matriz_productos, id_producto, columnas_productos.get("cantidad") - 1, -eleccion_cantidad)
 
 
-def mostrar_vendedores():
-    ''' Muestra al usuario una lista de todos los vendedores. '''
+def obtener_diccionario_columnas_ventas():
+    ''' Regresa un diccionario de las columnas en el archivo de texto de ventas. '''
 
-    lista_vendedores = open("vendedores.txt", "r")
+    diccionario = {"id": 1}
+    texto_ventas = open("ventas.txt", "r")
+    linea = texto_ventas.readline()
+    texto_ventas.close()
 
-    for linea in lista_vendedores:
-        if linea[0] == "#":
-            continue
+    linea = linea.split(",")
+
+    for i in range(len(linea) - 1):
+        diccionario[f"producto {i + 1}"] = i + 2
+
+    return diccionario
 
 
+# Declaración de diccionarios
 
-# Declaración de variables
+columnas_productos = {"id": 1, "modelo": 2, "nombre": 3, "cantidad": 4}
+columnas_vendedores = {"id": 1, "nombre": 2}
+columnas_ventas = obtener_diccionario_columnas_ventas()  # {"id": 1, "producto 1": 2, ..., "producto n": n + 1}
 
-pass
+# Declaración de matrices
+
+matriz_productos = pasar_texto_a_matriz("productos.txt")
+matriz_vendedores = pasar_texto_a_matriz("vendedores.txt")
+matriz_ventas = pasar_texto_a_matriz("ventas.txt")
 
 # Main
+
+print("Bienvenido al Administrador de Tienda")
 
 while True:
     mostrar_menu()
     eleccion = pedir_entero(1, 6, "Seleccione un menú (1-6)")
 
     if eleccion == 1:
-        pass
+        mostrar_menu_registrar_ventas()
     elif eleccion == 2:
         pass
     elif eleccion == 3:
